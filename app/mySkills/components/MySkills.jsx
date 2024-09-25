34d8b95c53a73ebb6aba2ae59b70e68d7f9497ad
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-26 00:00:17 +0530

    complete to my skills

--- app/mySkills/components/MySkills.jsx
+++ app/mySkills/components/MySkills.jsx
@@ -15,50 +15,71 @@ import Slider from "react-slick";
 
 const MySkills = () => {
   const mySkills = [
-    { id: 1, icon: faHtml5, value: "90%", name: "HTML", color: "#E34F26" }, // HTML color
-    { id: 2, icon: faCss3Alt, value: "90%", name: "CSS", color: "#1572B6" }, // CSS color
+    { id: 1, icon: faHtml5, value: "90%", name: "HTML", color: "#E34F26" },
+    { id: 2, icon: faCss3Alt, value: "90%", name: "CSS", color: "#1572B6" },
     {
       id: 3,
       icon: faJsSquare,
       value: "90%",
       name: "JavaScript",
       color: "#F7DF1E",
-    }, // JavaScript color
-    { id: 4, icon: faCode, value: "90%", name: "TypeScript", color: "#007ACC" }, // TypeScript color
-    { id: 5, icon: faReact, value: "90%", name: "React Js", color: "#61DAFB" }, // React color
-    { id: 6, icon: faReact, value: "90%", name: "Next Js", color: "#000000" }, // Next.js (black)
-    { id: 7, icon: faAngular, value: "90%", name: "Angular", color: "#DD0031" }, // Angular color
+    },
+    { id: 4, icon: faCode, value: "90%", name: "TypeScript", color: "#007ACC" },
+    { id: 5, icon: faReact, value: "90%", name: "React Js", color: "#61DAFB" },
+    { id: 6, icon: faReact, value: "90%", name: "Next Js", color: "#000000" },
+    { id: 7, icon: faAngular, value: "90%", name: "Angular", color: "#DD0031" },
     {
       id: 8,
       icon: faCogs,
       value: "90%",
       name: "Spring Boot",
       color: "#6DB33F",
-    }, // Spring Boot color
-    { id: 9, icon: faDatabase, value: "90%", name: "SQL", color: "#FF5733" }, // SQL color
+    },
+    { id: 9, icon: faDatabase, value: "90%", name: "SQL", color: "#FF5733" },
     {
       id: 10,
       icon: faDatabase,
       value: "90%",
       name: "Oracle",
       color: "#F80000",
-    }, // Oracle color
+    },
   ];
 
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
     arrows: false,
     responsive: [
       {
-        breakpoint: 3000,
+        breakpoint: 640,
         settings: {
-          slidesToShow: 6,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
           slidesToScroll: 1,
         },
       },
@@ -70,9 +91,16 @@ const MySkills = () => {
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1800,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 2000,
+        settings: {
+          slidesToShow: 6,
           slidesToScroll: 1,
         },
       },
@@ -81,13 +109,12 @@ const MySkills = () => {
 
   return (
     <div className="flex flex-col items-center py-20 gap-5 bg-[--skills-bg]">
-      <div>
+      <div className="">
         <TitleComponent props={{ title: "My Skills" }} />
       </div>
-      <div className="text-[--skills-text] text-xl max-w-[60%] text-center">
-        create mode 100644 app/myQualityServices/css/myQualityServiceTable.css
-        create mode 100644 app/myQualityServices/css/myQualityServiceTable.css
-        create mode 100644 app/myQ
+      <div className="text-[--skills-text] text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl max-w-[85%] text-center">
+        create mode 100644 app/myQualityServic create mode 100644 app/myQualityS
+        vices/css/myQu alityServic eTable.css
       </div>
       <div className="max-w-[80vw]">
         <Slider {...settings}>
--- app/mySkills/components/SkillCard.jsx
+++ app/mySkills/components/SkillCard.jsx
@@ -14,11 +14,16 @@ const SkillCard = ({ props }) => {
     >
       <div className="bg-[--skills-card-bg] hover:bg-[--skills-card-bg-hover] hover:scale-105 hover:border-2 hover:border-[--skills-card-border-hover] flex flex-col justify-center items-center gap-3 min-h-40 rounded-3xl transition-all duration-300 ease-out">
         <div>
-          <FontAwesomeIcon icon={props.icon} className="text-6xl" />
+          <FontAwesomeIcon
+            icon={props.icon}
+            className="text-6xl xl:text-7xl "
+          />
         </div>
-        <div className="text-2xl pl-2">{props.value}</div>
+        <div className="text-2xl  lg:text-3xl 2xl:text-4xl pl-2 font-bold">
+          {props.value}
+        </div>
       </div>
-      <div className="text-center text-xl text-[--skills-card-title]">
+      <div className="text-center text-xl  lg:text-2xl 2xl:text-3xl text-[--skills-card-title]">
         {props.name}
       </div>
     </div>
