a57bf94f7ed60b21b09e9ba622799a3e14c2e94b
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-26 00:05:38 +0530

    update' git push

--- app/qulification/components/Qualification.jsx
+++ app/qulification/components/Qualification.jsx
@@ -4,7 +4,7 @@ import Education from "./Education";
 
 const Qualification = () => {
   return (
-    <div className="flex flex-col lg:flex-row bg-[--qualification-bg] text-[--qualification-text] p-10 pb-20 justify-evenly">
+    <div className="flex flex-col lg:flex-row bg-[--qualification-bg] text-[--qualification-text] p-10 pb-20 justify-evenly gap-16 lg:gap-0">
       <Experience /> 
       <Education />
     </div>
