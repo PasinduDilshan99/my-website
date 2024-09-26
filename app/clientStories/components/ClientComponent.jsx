24c70c66dd36436a5f37c3c2fe963a4ed4e95c5f
Pasindu Dilshan Dimbulana <pasindudimbulana@gmail.com>
2024-09-26 20:13:50 +0530

    complete the client story part

--- app/clientStories/components/ClientComponent.jsx
+++ app/clientStories/components/ClientComponent.jsx
@@ -3,25 +3,34 @@ import LabelImportantIcon from "@mui/icons-material/LabelImportant";
 
 const ClientComponent = ({ props }) => {
   return (
-    <div className="bg-[--client-card-bg] mt-2 mb-7 rounded-lg p-4 gap-3 hover:shadow-xl hover:shadow-[--client-card-shadow-hover] flex flex-col  transition-all duration-300 ease-in-out hover:scale-105">
+    <div className="bg-[--client-story-card-bg] mt-2 mb-7 rounded-lg p-4 gap-3 hover:shadow-xl hover:shadow-[--client-story-card-shadow-hover] flex flex-col  transition-all duration-300 ease-in-out hover:scale-105">
       <div className="flex justify-between">
-        <div className="flex gap-2 text-white">
+        <div className="flex gap-2 text-[--client-story-card-company-logo] text-lg  md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
           <div>
             <props.companyLogo />
           </div>
           <div>{props.companyName}</div>
         </div>
+
         <div>
           <img src={props.image} width={50} height={50} alt="image" />
         </div>
       </div>
-      <div className="text-[--client-card-icon] ">
-        <LabelImportantIcon className="text-3xl" />
+
+      <div className="text-[--client-story-card-icon] ">
+        <LabelImportantIcon className="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl" />
       </div>
-      <div className="text-lg text-[--client-text]">{props.description}</div>
-      <div className="flex flex-col text-lg text-[--client-text]">
-        <div className="font-semibold text-white">{props.name}</div>
-        <div>{props.position}</div>
+
+      <div className="text-md lg:text-lg xl:text-xl l text-[--client-story-text]">
+        {props.description}
+      </div>
+      <div className="flex flex-col text-[--client-story-text]">
+        <div className="font-semibold text-[--client-story-card-name] text-lg md:text-xl  xl:text-2xl 2xl:text-3xl">
+          {props.name}
+        </div>
+        <div className=" text-sm md:text-md lg:text-lg xl:text-xl ">
+          {props.position}
+        </div>
       </div>
     </div>
   );
--- app/clientStories/components/ClientStories.jsx
+++ app/clientStories/components/ClientStories.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import "../css/clientStories.css";
 import { clientDetails } from "../../../assets/clientDetails";
@@ -12,7 +12,7 @@ const ClientStories = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 7000,
+    autoplaySpeed: 5000,
     pauseOnHover: true,
     arrows: false,
     responsive: [
@@ -41,19 +41,19 @@ const ClientStories = () => {
   };
 
   return (
-    <div className="min-h-[50vh] bg-black clientStoriesBubble p-10 pt-20 flex justify-between">
+    <div className="min-h-[50vh] clientStoriesBubble p-10 pt-20 flex lg:flex-row flex-col lg:justify-between ">
       <div className="flex flex-col gap-5">
-        <div className="clientTitle text-5xl font-bold">
+        <div className="clientTitle text-3xl  md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold">
           My Client's Stories
         </div>
-        <div className="text-white text-lg">
+        <div className="text-[--client-stories-text] text-md  md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Empowering people in new a digital journey with my super services
         </div>
       </div>
-      <div className="max-w-[40vw] min-w-[50%]">
+      <div className="lg:max-w-[40vw] lg:min-w-[50%] mt-5">
         <Slider {...settings}>
           {clientDetails.map((clientDetail) => (
-            <div key={clientDetail.id} className="px-4 ">
+            <div key={clientDetail.id} className="px-4">
               <ClientComponent props={clientDetail} />
             </div>
           ))}
